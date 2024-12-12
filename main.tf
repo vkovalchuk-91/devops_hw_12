e1bc003c3ea9d85362231e62ba3556ca1a651b35
Volodymyr Kovalchuk <102913549+vkovalchuk-91@users.noreply.github.com>
2024-12-12 18:33:09 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,125 @@
+terraform {
+  backend "s3"{
+  bucket = "github-actions-slengpack"
+  key = "terrraform.tfstate"
+  region = "eu-central-1"
+  }
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "HW_12_VPC"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "HW_12_Internet-Gateway"
+  }
+}
+
+
+resource "aws_subnet" "public" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet("10.0.0.0/20", 8, count.index)
+  availability_zone = element(["eu-central-1a", "eu-central-1b", "eu-central-1c"], count.index)
+
+  tags = {
+    Name = "HW_12_Public-Subnet-${count.index + 1}"
+  }
+}
+
+
+resource "aws_subnet" "private" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet("10.0.16.0/20", 8, count.index)
+  availability_zone = element(["eu-central-1a", "eu-central-1b", "eu-central-1c"], count.index)
+
+  tags = {
+    Name = "HW_12_Private-Subnet-${count.index + 1}"
+  }
+}
+
+
+resource "aws_subnet" "database" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet("10.0.32.0/20", 8, count.index)
+  availability_zone = element(["eu-central-1a", "eu-central-1b", "eu-central-1c"], count.index)
+
+  tags = {
+    Name = "HW_12_Database-Subnet-${count.index + 1}"
+  }
+}
+
+
+resource "aws_route_table" "public" {
+  count  = 3
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "HW_12_Public-Route-Table-${count.index + 1}"
+  }
+}
+
+resource "aws_route" "public" {
+  count                  = 3
+  route_table_id         = aws_route_table.public[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 3
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
+}
+
+
+resource "aws_route_table" "private" {
+  count  = 3
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "HW_12_Private-Route-Table-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 3
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
+
+
+resource "aws_route_table" "database" {
+  count  = 3
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "HW_12_Database-Route-Table-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "database" {
+  count          = 3
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database[count.index].id
+}
